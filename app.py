db9df1fb4d128c7e1bc543f0ef0e024453ba885b
Hiroyuki Chu <chu22f@gmail.com>
2019-04-10 17:35:08 +0800

    added login API

--- app.py
+++ app.py
@@ -1,18 +1,31 @@
+from flask_wtf.csrf import CSRFProtect
 import os
 import config
 from flask import Flask
 from models.base_model import db
 
+from flask_jwt_extended import (
+    JWTManager, jwt_required, create_access_token,
+    get_jwt_identity
+)
+
 web_dir = os.path.join(os.path.dirname(
     os.path.abspath(__file__)), 'instagram_web')
 
-app = Flask('NEXTAGRAM', root_path=web_dir)
+app = Flask('Barter', root_path=web_dir)
+
+app.config['JWT_SECRET_KEY'] = 'super-secret'  # Change this!
+jwt = JWTManager(app)
+
+
+app = Flask('Barter', root_path=web_dir)
 
 if os.getenv('FLASK_ENV') == 'production':
     app.config.from_object("config.ProductionConfig")
 else:
     app.config.from_object("config.DevelopmentConfig")
 
+csrf = CSRFProtect(app)
 
 @app.before_request
 def before_request():
--- instagram_api/blueprints/users/views.py
+++ instagram_api/blueprints/users/views.py
@@ -1,4 +1,17 @@
 from flask import Blueprint
+from flask import Blueprint, request,jsonify
+from flask import render_template,redirect,request,url_for,session,flash,escape
+import os
+
+import re
+from models.user import User
+from werkzeug.security import generate_password_hash,check_password_hash
+from flask_jwt_extended import (
+    create_access_token,
+)
+
+from app import csrf
+
 
 users_api_blueprint = Blueprint('users_api',
                              __name__,
@@ -7,3 +20,55 @@ users_api_blueprint = Blueprint('users_api',
 @users_api_blueprint.route('/', methods=['GET'])
 def index():
     return "USERS API"
+
+
+@users_api_blueprint.route('/new', methods=['POST'])
+@csrf.exempt
+def create():
+    if not request.is_json:
+        return jsonify({"msg": "Missing JSON in request"}), 400
+
+    username = request.json.get('username', None)
+    password = request.json.get('password', None)
+    email = request.json.get('email',None)
+    
+
+    if not username:
+        return jsonify({"msg": "Missing username parameter"}), 400
+    if not password:
+        return jsonify({"msg": "Missing password parameter"}), 400
+    if not email:
+        return jsonify({"msg": "Missing email parameter"}), 400
+
+    # actual sign up users 
+    user_password = password
+    hashed_password = generate_password_hash(user_password)
+
+    pattern_password = '\w{6,}'
+    result = re.search(pattern_password, user_password)
+    pattern_email = '[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]'
+    result_email = re.search(pattern_email,email)
+
+    username_check = User.get_or_none(User.username == username)
+    email_check = User.get_or_none(User.email == email)
+    
+    if (result and result_email):
+        u = User(username=username,email=email,password=hashed_password)
+
+    if not username_check and not email_check:
+        u.save()
+        user = User.get(User.username == username)
+        access_token = create_access_token(identity=username)
+        return jsonify({
+        "access_tokhttp://localhost:5000/api/v1/auth/loginen": access_token,
+        "message": "Successfully created a user and signed in.",
+        "status": "success",
+        "user": {
+            "id": user.id,
+            "profile_picture": user.profile_image_url,
+            "username": user.username
+        }
+    }), 200
+    else:
+        return jsonify({"msg": "username or email already used"}), 400
+  
